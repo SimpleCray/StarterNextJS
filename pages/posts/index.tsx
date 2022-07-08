30c09c45504e6659633ce44ac6cdcdcc035d3c05
Hai Duong <hai.duong@kodebaze.com>
2022-07-08 11:15:23 +0700

    getStaticProps function

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,36 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import React from 'react';
 
-export interface IPostListPageProps {}
-
-export default function PostListPage(props: IPostListPageProps) {
-	return <div>Post List page</div>;
+export interface IPostListPageProps {
+	posts: any[];
 }
+
+export default function PostListPage({ posts }: IPostListPageProps) {
+	return (
+		<div>
+			<h1>Post List page</h1>
+
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export const getStaticProps: GetStaticProps<IPostListPageProps> = async (
+	context: GetStaticPropsContext
+) => {
+	// Run on server at build time
+	// console.log('static props');
+	const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1');
+	const data = await response.json();
+	// console.log(data);
+
+	return {
+		props: {
+			posts: data.data,
+		},
+	};
+};
